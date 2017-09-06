24a959f36bdc0c82351546cec223db3c2937e1d8
Richard Rodger <richard@ricebridge.com>
2017-09-06 11:26:54 +0100

    client-side working

--- www/vue-app.js
+++ www/vue-app.js
@@ -13,7 +13,9 @@ var vm1 = new Vue({
   watch: {
     query() {
       seneca.act({role:'web', cmd:'query', query:this.$data.query}, function(err, out) {
-        console.log(err, out)
+        if(out && out.items) {
+          seneca.act({ann:'results', results:out.items})
+        }
       })
     },
 
@@ -67,6 +69,15 @@ var vm2 = new Vue({
   el: '#results',
   data: {
     results: ''
+  },
+  beforeCreate: function () {
+    var self = this
+    
+    seneca
+      .add('ann:results', function(msg, reply) {
+        self.results = msg.results
+        reply()
+      })
   },
   methods: {
 
