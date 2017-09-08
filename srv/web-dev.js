6ae2571d12637d140e788ccde09b090372eb99d0
davidwroche <davidwroche@yahoo.co.uk>
2017-09-08 15:01:46 +0100

    Info page now works and returns matches if found

--- srv/web-dev.js
+++ srv/web-dev.js
@@ -30,14 +30,31 @@ Seneca({tag: 'web', legacy: {transport: false}})
 
   .add('role:web,cmd:sug', function (msg, reply) {
     this.act('role:suggest,cmd:suggest',{query:msg.query,default$:[]},
-            function(err,out){
-            reply(out||[])
+            function(err,sug){
+            reply(null,{sug})
       })
   })
 
   .add('role:web,cmd:query', function (msg, reply) {
     this.act({role:'search', cmd:'search', query:msg.query}, reply)
   })
+
+  .add('role:web,cmd:info', function (msg, reply){
+    this.act({role:'info',cmd:'get',name:msg.name},
+      function (err, info) {
+            if( err ) {
+              info = {}
+            }
+            //reply({mod:'hi'})
+
+            info.no_npm = !info.npm
+            info.no_github = !info.github
+
+            reply(info)
+          })
+  })
+
+
 
   .ready(function(){
     var server = init_hapi({seneca: this})
--- www/js/vue/vue-app.js
+++ www/js/vue/vue-app.js
@@ -11,6 +11,8 @@ var vm1 = new Vue({
   el: '#search',
   data: {
     query: '',
+    info: false,
+    layout1: true
   },
   watch: {
     query() {
@@ -20,11 +22,10 @@ var vm1 = new Vue({
         cmd: 'sug',
         query: this.$data.query
       }, function(err, result) {
-        console.log(result)
         if (result) {
           seneca.act({
             omar: 'suggestions',
-            suggestions: result
+            suggestions: result.sug
           })
         }
       })
@@ -52,6 +53,23 @@ var vm1 = new Vue({
         claire: 'clear_results',
         results: ''
       })
+    },
+    layout() {
+      this.layout1 = false;
+      vm2.$data.layout2 = false;
+      vm3.$data.layout3 = false;
+      vm4.$data.layout4 = true;
+    },
+    revertLayout() {
+      this.layout1 = true;
+      vm2.$data.layout2 = true;
+      vm3.$data.layout3 = true;
+      vm4.$data.layout4 = false;
+    }
+  },
+  mounted: function() {
+    if (window.origin.toString() === 'http://' + 'localhost:8000') {
+      this.info = true;
     }
   }
 })
@@ -60,7 +78,8 @@ var vm1 = new Vue({
 var vm2 = new Vue({
   el: '#results',
   data: {
-    results: ''
+    results: '',
+    layout2: true
   },
   beforeCreate: function() {
     var self = this
@@ -69,12 +88,12 @@ var vm2 = new Vue({
       .add('ann:results', function(msg, reply) {
         self.results = msg.results
         self.results.forEach(function(item) {
-          if(!item.hasOwnProperty(item.giturl)){
+          if (!item.hasOwnProperty(item.giturl)) {
             item.npmurl = "http://npmjs.com/package/" + item.name.toString()
             item.namelink = item.npmurl
           }
 
-          if('giturl'in item){
+          if ('giturl' in item) {
             if (item.giturl.startsWith('git://')) {
               var x = item.giturl.replace('git://', 'http://')
               item.giturl = x
@@ -85,7 +104,7 @@ var vm2 = new Vue({
               item.giturl = y
               item.namelink = item.giturl
             }
-          }else{
+          } else {
             item.npmurl = "http://npmjs.com/package/" + item.name.toString()
             item.namelink = item.npmurl
           }
@@ -108,21 +127,61 @@ var vm2 = new Vue({
 var vm3 = new Vue({
   el: '#suggest',
   data: {
-    suggest: ''
+    suggest: '',
+    layout3: true
   },
   beforeCreate: function() {
     var self = this;
     seneca
       .add('omar:suggestions', function(msg, reply) {
         self.suggest = msg.suggestions
+        var list = document.getElementById('suggestlist');
+        document.getElementById('suggestlist').innerHTML = '';
+        self.suggest.forEach(function(item) {
+          var option = document.createElement('option');
+          option.value = item;
+          list.appendChild(option);
+        });
         reply()
-        //   var list = document.getElementById('suggestlist');
-        //   document.getElementById('suggestlist').innerHTML = '';
-        //   self.suggest.forEach(function(item){
-        //   var option = document.createElement('option');
-        //   option.value = item.name;
-        //   list.appendChild(option);
-        // });
+      })
+  }
+})
+
+
+// Start of Info Vue Instance
+var vm4 = new Vue({
+  el: '#info',
+  data: {
+    npm: '',
+    github: '',
+    name: '',
+    information: '',
+    layout4: false
+  },
+  methods: {
+    getInfo() {
+      seneca.act({
+        role: 'web',
+        cmd: 'info',
+        name: this.$data.name
+      }, function(err, mod) {
+        if (mod) {
+          seneca.act({
+            ian: 'get_info',
+            name: mod
+          });
+        }
+      });
+    }
+  },
+  beforeCreate: function() {
+    var self = this;
+    seneca
+      .add('ian:get_info', function(msg, reply) {
+        console.log(msg.name.github)
+        self.github = msg.name.github
+        self.npm = msg.name.npm
+        reply()
       })
   }
 })
