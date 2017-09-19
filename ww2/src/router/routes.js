f492b4500b98bed894e0f7dd59007f5115f7e6f5
davidwroche <davidwroche@yahoo.co.uk>
2017-09-19 16:12:27 +0100

    Removed old ww2 which had single file components

--- ww2/src/router/routes.js
+++
@@ -1,8 +0,0 @@
-// import Results from '.././components/results.vue';
-// import Search from '.././components/search.vue';
-//
-//
-// export const routes = [
-//   { path: '/results', component: Results },
-//   { path: '/search', component: Search },
-// ];
